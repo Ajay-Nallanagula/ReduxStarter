4c50f4afe3cbffcf037e83890d3e58071a7355a7
AjayNallanagula <kumarnajay9@gmail.com>
2021-01-21 11:44:40 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/*
+We are Trying to build bug tracking system 
+Actions that can be done 
+1) We can add a bug
+2) We can delete a bug
+3) We can resolve a bug, changing boolean property to true 
+*/
